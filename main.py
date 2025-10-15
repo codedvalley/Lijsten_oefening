ef2ede8601c87fb044dc156441102778cb559f6c
codedvalley <codedvalley@gmail.com>
2025-10-15 21:56:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+tafel_van_drie = [3, 6, 9, 12, 16, 18, 24, 27, 32]
+tafel_van_drie[4] = 15
+tafel_van_drie.remove(32)
+tafel_van_drie.append(30)
+tafel_van_drie.insert(6, 21)
+print(tafel_van_drie)
+
+# Opgave
+# Maak de lijst tafel_van_drie = [3, 6, 9, 12, 16, 18, 24, 27, 32] aan. Je ziet al dat er een aantal waardes niet kloppen.
+#
+# Gebruik een toekenning om 16 in 15 te veranderen
+# Gebruik de methode remove() om 32 te verwijderen
+# Gebruik de methode append() om 30 toe te voegen aan het eind van de lijst
+# Gebruik de methode insert() om 21 toe te voegen tussen 18 en 24
